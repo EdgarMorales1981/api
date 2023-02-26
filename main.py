f9197d57e18ae4d4c5dfd51cc8f8f5e5c6185392
Edgar Jose Morales Oses <48526693+EdgarMorales1981@users.noreply.github.com>
2023-02-26 14:18:21 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
+from fastapi import FastAPI, UploadFile,Form
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 import psycopg2
-
-import uuid
 import cloudinary
 import cloudinary.uploader
-import uvicorn
-from fastapi import FastAPI, status, UploadFile, Form
-from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware
-
-class Servicio(BaseModel):
-    id: str = None
-    nombre: str
-    descripcion: str
-    foto: str
-    precio: str
-
+import uuid
+headers={"content-type":"utf-8"}
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
@@ -31,33 +23,17 @@ cloudinary.config(
  )
 
 
-@app.get("/status")
-def check_status():
-    return {"status": "ok"}
-
-@app.get("/servicios{id}",status_code=status.HTTP_200_OK)
-async def serviciosOne(id:str):
-    conn = psycopg2.connect(
-        database="vfvxprgv",
-        user='vfvxprgv',
-        password='u1Ququtd0f-27eKTgXwzlrHTpwaYqVUE',
-        host="mahmud.db.elephantsql.com"
-    )
-    cur = conn.cursor()
-    cur.execute(f"SELECT * FROM contenedor WHERE ID = '{id}' ")
-    rows = cur.fetchall()
-
-    formatted_datos = []
-    for row in rows:
-        formatted_datos.append(
-            Servicio(id=row[0], nombre=row[1], descripcion=row[2],foto=row[3],precio=row[4])
-        )
-
-    cur.close()
-    conn.close()
-    return formatted_datos
-
-@app.get("/servicios",status_code=status.HTTP_200_OK)
+class Servicio(BaseModel):
+    id: str = None
+    nombre: str
+    descripcion: str
+    foto: str
+    precio: str
+@app.get("/")
+async def index():
+    content = {"message": "!Hello World"}
+    return JSONResponse(content=content,headers=headers)
+@app.get("/servicios")
 async def servicios():
     conn = psycopg2.connect(
         database="vfvxprgv",
@@ -67,61 +43,58 @@ async def servicios():
     )
     cur = conn.cursor()
     cur.execute("SELECT * FROM contenedor ORDER BY id DESC")
-    rows = cur.fetchall()
-
-    formatted_datos = []
-    for row in rows:
-        formatted_datos.append(
-            Servicio(id=row[0], nombre=row[1], descripcion=row[2],foto=row[3],precio=row[4])
-        )
-
+    rows= cur.fetchall()
+    content = [{
+        "id": row[0],
+        "nombre": row[1],
+        "descripcion": row[2],
+        "foto": row[3],
+        "precio": row[4]
+    } for row in  rows]
     cur.close()
     conn.close()
-    return formatted_datos
+    return JSONResponse(content=content,headers=headers,media_type="application/json")
 
-@app.post("/servicios",status_code=status.HTTP_201_CREATED)
-async def create_servicio(file:UploadFile, nombre:str = Form(), descripcion: str = Form(), precio: str = Form()):
-        result = cloudinary.uploader.upload(file.file)
-        url =result.get("url")
-        id = uuid.uuid4()
-        foto = url
-        conn = psycopg2.connect(
-            database="vfvxprgv",
-            user='vfvxprgv',
-            password='u1Ququtd0f-27eKTgXwzlrHTpwaYqVUE',
-            host="mahmud.db.elephantsql.com"
-        )
-        cur = conn.cursor()
-        cur.execute(f"INSERT INTO contenedor (id, nombre,descripcion, foto, precio) VALUES  ('{id}','{nombre}','{descripcion}','{foto}','{precio}')")
-        conn.commit()
-        cur.close()
-        conn.close()
-        return {"id": id, "nombre": nombre, "descripcion": descripcion, "foto": foto, "precio": precio}
+@app.get("/servicios/buscar/{id}")
+async def servicios_buscar(id: str):
+    conn = psycopg2.connect(
+    database="vfvxprgv",
+    user='vfvxprgv',
+     password='u1Ququtd0f-27eKTgXwzlrHTpwaYqVUE',
+    host="mahmud.db.elephantsql.com"
+    )
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM contenedor WHERE id = '{id}'")
+    rows = cur.fetchall()
+    content = [{
+        "id": row[0],
+        "nombre": row[1],
+        "descripcion": row[2],
+        "foto": row[3],
+        "precio": row[4]
+    } for row in rows]
+    cur.close()
+    conn.close()
+    return JSONResponse(content=content, headers=headers, media_type="application/json")
 
-
-@app.put("/servicios/{id}",status_code=status.HTTP_200_OK)
-async def editar(id:str, file:UploadFile, nombre:str = Form(), descripcion: str = Form(), precio: str = Form()):
+@app.post("/servicios/crear")
+async def crear_servicio(file: UploadFile, nombre: str = Form(), descripcion: str = Form(), precio: str = Form()):
     result = cloudinary.uploader.upload(file.file)
-    url = result.get("url")
-    foto=url
+    url = result.get("secure_url")
+    id = uuid.uuid4()
+    foto = url
     conn = psycopg2.connect(
         database="vfvxprgv",
         user='vfvxprgv',
         password='u1Ququtd0f-27eKTgXwzlrHTpwaYqVUE',
         host="mahmud.db.elephantsql.com"
     )
-
-
     cur = conn.cursor()
     cur.execute(
-        f"UPDATE contenedor SET nombre='{nombre}',descripcion='{descripcion}', foto='{foto}', precio='{precio}' WHERE id='{id}'")
+        f"INSERT INTO contenedor (id, nombre,descripcion, foto, precio) VALUES  ('{id}','{nombre}','{descripcion}','{foto}','{precio}')")
     conn.commit()
     cur.close()
     conn.close()
-    return  {"id": id, "nombre": nombre, "descripcion": descripcion, "foto": foto, "precio": precio}
+    return JSONResponse(content={"message": "Servicio creado"}, headers=headers, media_type="application/json")
 
 
-
-
-if __name__ == "__main__":
-     uvicorn.run(app, host="0.0.0.0", port=8000)
